6a0cfd424d70d0a4c899ae3e33eb4ec2a2108f0e
Alexey Seleznev <rib@live.ru>
2025-02-24 04:49:17 +0300

    fixed ChainLoggerTest

--- src/test/java/ru/r1b/logger/ChainLoggerTest.java
+++ src/test/java/ru/r1b/logger/ChainLoggerTest.java
@@ -3,14 +3,19 @@ package ru.r1b.logger;
 import ru.r1b.logger.formatter.LeveledLog;
 import ru.r1b.logger.logger.ChainLogger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ChainLoggerTest extends LoggerTest {
     @Override
     Logger getLogger() {
 
         return new ChainLogger(
-                channelMock,
+                (ArrayList<Channel>) Collections.singletonList(channelMock),
                 LogLevel.INFO,
-                new LeveledLog(LogLevel.INFO)
+                (ArrayList<Formatter>) Collections.singletonList((Formatter) new LeveledLog(LogLevel.INFO))
+
         );
     }
 }
